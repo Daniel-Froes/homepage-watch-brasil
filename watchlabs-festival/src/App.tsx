e8f1694b7ffa5d2c82cd1adb4d0f99d495757f54
daniel nivello <daniel.froes@nivello.com.br>
2026-01-20 17:15:32 -0300

    chore: setup tailwind css

---
+++ watchlabs-festival/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background ">
+      <h1 className="text-2xl font-bold p-6">
+        W Festival
+      </h1>
+    </div>
+  );
+}
