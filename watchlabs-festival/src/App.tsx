d36f2ce93f6be5095121e686c7e3b99d56e16965
art <89751087+Daniel-Froes@users.noreply.github.com>
2026-01-20 18:07:12 -0300

    Delete watchlabs-festival directory

--- watchlabs-festival/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background ">
-      <h1 className="text-2xl font-bold p-6">
-        W Festival
-      </h1>
-    </div>
-  );
-}
